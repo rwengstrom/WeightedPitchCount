20d33fdab025e767d55cce58b6653897fcd806bf
rwengstrom <79719265+rwengstrom@users.noreply.github.com>
2021-04-02 13:26:40 -0700

    Data Upload and Merge

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 
 import pandas as pd
 
+# Load in Necessary Datasets
 pitches = pd.read_csv('pitches.csv')
 leverage = pd.read_csv('leverage.csv')
+
+# Merge and Clean Data
+df = pd.merge(pitches,leverage)
+df = df.drop(['code','type','event_num','on_1b','on_2b','on_3b'], axis = 1)
+print(df.columns)
