f6d9f9d22a868379973b6ce4db591bff0a5df951
rwengstrom <79719265+rwengstrom@users.noreply.github.com>
2021-04-16 10:24:06 -0700

    Added Weighted Pitch Count

--- main.py
+++ main.py
@@ -2,11 +2,13 @@
 import pandas as pd
 
 # Load in Necessary Datasets
-pitches = pd.read_csv('data/pitches.csv')
-leverage = pd.read_csv('data/leverage.csv')
+pitches = pd.read_csv('pitches.csv')
+leverage = pd.read_csv('leverage.csv')
+atbats = pd.read_csv('atbats.csv')
 
 # Merge and Clean Data
 df = pd.merge(pitches,leverage)
+df = pd.merge(df, atbats)
 df = df.drop(['code','type','event_num','on_1b','on_2b','on_3b'], axis = 1)
 print(df.columns)
 
@@ -20,6 +22,10 @@ ball = df[['pitch_type',
 pitcher = ball.groupby('pitcher_id')
 ave_lev_pitch = pd.DataFrame((pitcher.sum()['pitch_leverage'])/(pitcher.count()['pitch_leverage']))
 
-games = pd.read_csv('data/2019_atbats.csv') #These are 2019 games
-
 ball.sort_values(by = 'ab_id') #These are pitches from 2015-2018
+
+pitch = df[['pitcher_id','g_id','outs','inning','pitch_leverage']]
+
+pitch['weighted_pitch_count'] = pitch.groupby(['g_id','pitcher_id'])['pitch_leverage'].cumsum()
+
+print(pitch.head())
